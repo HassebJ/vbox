2c88516240f18b87520d87225053cd7cb0d299fd
HassebJ <11besemjaved@seecs.edu.pk>
2015-12-30 02:22:49 +0500

    fixed mail password

--- email/index.js
+++ email/index.js
@@ -1,10 +1,12 @@
 // CONNECT to Mandrill
+
+var mailer = require("nodemailer").mail;
 mail = nodemailer.createTransport("SMTP", {
-	host	: "mail.livevbox.com", 	// smtp hostname
+	host	: "webmail.gandi.net", 	// smtp hostname
 	port	: 25, 						// port for secure smtp
 	debug	: true,						// display debug log
     auth	: {
-        user: "hello@livevbox.com",		// smtp username
+        user: "hello@veebox.io",		// smtp username
         pass: "vbox@1234"	// smtp password
     }
 });
@@ -14,14 +16,22 @@ mail.send = function(locals, options){
 	// Render HTML Template
 	var options = options || {};
 	app.ect.render(__dirname + '/templates/' + locals.html, locals, function (error, html) {
-		if(!isset(error)){ 
-			mail.sendMail(merge({
-			    from	: "VBOX <hello@livevbox.com>",
-			    to		: locals.email,
-			    subject	: locals.subject,
-			    html	: html,
-			    generateTextFromHTML: true
-			}, options));
+		if(!isset(error)){
+
+            mailer({
+                from: "VBOX <hello@veebox.io>", // sender address
+                to: locals.email, // list of receivers
+                subject: locals.subject, // Subject line
+                html: html // html body
+//                generateTextFromHTML: true
+            });
+//			mail.sendMail(merge({
+//			    from	: "VBOX <hello@veebox.io>",
+//			    to		: locals.email,
+//			    subject	: locals.subject,
+//			    html	: html,
+//			    generateTextFromHTML: true
+//			}, options));
 		} else {
 			console.trace('Mail ECT Rendering Error: ', error);
 		}
