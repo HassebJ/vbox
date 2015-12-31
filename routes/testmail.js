20048d660972475ddc2d9d6f07fe4d5f0cc34448
HassebJ <11besemjaved@seecs.edu.pk>
2015-12-31 17:07:43 +0500

    fixed minor front-end issue and added contact form submission

--- routes/testmail.js
+++ routes/testmail.js
@@ -1,3 +1,4 @@
+var mailer = require("nodemailer").mail;
 app.get('/testmail', function(request, response, mysql){
 		mail.send({
 					email: 'deb17276@gmail.com',
@@ -10,4 +11,25 @@ app.get('/testmail', function(request, response, mysql){
 		response.data.page = 'welcome';
 		response.data.scripts = [scripts.maps, scripts.geo, scripts.validetta, scripts.page(response)];
 		response.finish();
+});
+
+app.get('/company/postcontact', function(request, response, mysql){
+    var msg = request.query;
+    console.log("query: " + msg.cf_name +' <'+msg.cf_email+'>');
+    console.log(msg.cf_subject);
+    console.log(msg.cf_message);
+
+    mailer({
+        from: msg.cf_name +' <'+msg.cf_email+'>', // sender address
+        to: 'leoash842@hotmail.com', // list of receivers
+        subject: msg.cf_subject, // Subject line
+        text: msg.cf_message // html body
+//                generateTextFromHTML: true
+    });
+
+    response.data.title = 'VBOX';
+    response.data.page = 'submitcontact';
+    response.finish();
+
+
 });
