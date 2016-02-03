d526ce747f2dafea9f26c5dab259265e5b43a0e9
Qasim Zubair <babarkhan7311@gmail.com>
2016-02-03 12:03:02 +0500

    Update easy.js

--- easy.js
+++ easy.js
@@ -370,6 +370,7 @@ User.prototype = {
         });
     },
     saveUserChat: function(from, to, text){
+    	console.log("INSERT INTO `chat`(`from`, `to`, `message`) VALUES("+connection.escape(from)+", "+connection.escape(to)+", "+connection.escape(text)+");
         pool.getConnection(function(err, connection) {
             connection.query("INSERT INTO `chat`(`from`, `to`, `message`) VALUES("+connection.escape(from)+", "+connection.escape(to)+", "+connection.escape(text)+")", function(err, result){
             connection.release();
